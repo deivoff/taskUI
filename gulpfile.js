460e3d87148c052da7eb7ebcbc651629dcc46f0b
Evgeny Mazurov <deivoff@gmail.com>
2018-08-23 10:45:53 +0500

    Настроил галп для релоадда и прочих плюшек

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var pug = require('gulp-pug');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
 var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
@@ -9,12 +10,22 @@ var uglify = require('gulp-uglify');
 var plumber = require('gulp-plumber');
 var babel = require('gulp-babel');
 
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: 'build/'
+        },
+        notify: false
+    })
+});
+
 gulp.task('css', function() {
     return gulp.src('src/templates/*.sass')
     .pipe(sass().on('error', sass.logError))
     .pipe(prefixer())
     .pipe(minifyCSS())
     .pipe(gulp.dest('build/css'))
+    .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('html', function() {
@@ -23,6 +34,7 @@ gulp.task('html', function() {
         pretty: true
     }))
     .pipe(gulp.dest('build'))
+    .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('js', function() {
@@ -38,3 +50,9 @@ gulp.task('js', function() {
 });
 
 gulp.task('default', [ 'css', 'html', 'js']);
+
+gulp.task('watch', ['browserSync', 'default'], function() {
+    gulp.watch('src/templates/*.sass', ['css']);
+    gulp.watch('src/templates/*.pug', ['html']);
+    gulp.watch('src/javascript/*.js', ['js']);
+})
